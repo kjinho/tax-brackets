b0fb4cfce5ac278fe52fdc886386a7caa155cad9
Jin-Ho King <j@kingesq.us>
2024-11-26 18:45:30 -0500

    elm-format

--- src/Main.elm
+++ src/Main.elm
@@ -1,142 +1,214 @@
-module Main exposing (Model, Msg, update, view, main)
+module Main exposing (Model, Msg, main, update, view)
 
-import TaxData exposing (..)
-import Parser
 import Browser
-import Html exposing (Html, div, input, text, table, caption, tbody, tr, th, td)
-import Html.Events exposing (onInput)
+import Html exposing (Html, caption, div, input, table, tbody, td, text, th, tr)
 import Html.Attributes exposing (class, placeholder)
-
+import Html.Events exposing (onInput)
 import Numeral exposing (format)
+import Parser
+import TaxData exposing (..)
+
 
 main : Program () Model Msg
 main =
-    Browser.element { init = init
-                     , view = view
-                     , update = update
-                     , subscriptions = subscriptions
-                     }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
 
 -- MODEL
 
+
 type alias Model =
-    { content: Maybe Int
+    { content : Maybe Int
     }
 
-init : () -> (Model, Cmd Msg)
-init _ = ( { content = Just 100}
-         , Cmd.none
-         )
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { content = Just 100 }
+    , Cmd.none
+    )
+
 
 type Msg
     = Change String
 
+
 parseInput : String -> Model
 parseInput str =
     let
-        cleanInput = str
-                     |> String.replace "$" ""
-                     |> String.replace "," ""
-                     |> Parser.run Parser.float
+        cleanInput =
+            str
+                |> String.replace "$" ""
+                |> String.replace "," ""
+                |> Parser.run Parser.float
     in
-        case cleanInput of
-            Ok f -> { content = Just (round f) }
-            Err _ -> { content = Nothing }
-      
+    case cleanInput of
+        Ok f ->
+            { content = Just (round f) }
+
+        Err _ ->
+            { content = Nothing }
+
+
 update : Msg -> b -> ( Model, Cmd msg )
 update msg _ =
     case msg of
-        Change newContent -> ( parseInput newContent , Cmd.none )
+        Change newContent ->
+            ( parseInput newContent, Cmd.none )
+
+
 
 -- VIEWS
+
 
 modelToTaxesString : Model -> String
 modelToTaxesString model =
     case model.content of
         Just f ->
             calculateTaxes taxBracket2023single (f - standardDeduction2023single)
-            |> (\x -> "$"++formatInt x)
-        Nothing -> "Bad Input"
+                |> (\x -> "$" ++ formatInt x)
+
+        Nothing ->
+            "Bad Input"
+
 
 modelToTaxRateString : Model -> String
 modelToTaxRateString model =
     case model.content of
         Just f ->
             calculateEffectiveTaxRate taxBracket2023single standardDeduction2023single f
-            |> (\x -> format "0.0%" x)
-        Nothing -> "Bad Input"
+                |> (\x -> format "0.0%" x)
+
+        Nothing ->
+            "Bad Input"
+
 
 view : Model -> Html Msg
-view model = 
+view model =
     viewHelper model
+
 
 viewHelper : Model -> Html.Html Msg
 viewHelper model =
     div []
-        [ mainTable [class "table"] model ]
+        [ mainTable [ class "table" ] model ]
+
 
 mainTable : List (Html.Attribute Msg) -> Model -> Html.Html Msg
-mainTable attributes model = 
-    table attributes [ caption [] [ text "Tax Year 2023" 
-                          , tbody [] [ tr [] [ th [] [text "Annual Income"]
-                                             , td [] [input [ placeholder "Income"
-                                                     , onInput Change ] [] ]]
-                                     , trRow [] [ text "Federal Income Tax" 
-                                                , text (modelToTaxesString model) ]
-                                     , trRow [] [ text "Effective Federal Tax Rate"
-                                                , text (modelToTaxRateString model) ]]]]
+mainTable attributes model =
+    table attributes
+        [ caption []
+            [ text "Tax Year 2023"
+            , tbody []
+                [ tr []
+                    [ th [] [ text "Annual Income" ]
+                    , td []
+                        [ input
+                            [ placeholder "Income"
+                            , onInput Change
+                            ]
+                            []
+                        ]
+                    ]
+                , trRow []
+                    [ text "Federal Income Tax"
+                    , text (modelToTaxesString model)
+                    ]
+                , trRow []
+                    [ text "Effective Federal Tax Rate"
+                    , text (modelToTaxRateString model)
+                    ]
+                ]
+            ]
+        ]
+
 
 trRow : List (Html.Attribute msg) -> List (Html.Html msg) -> Html.Html msg
 trRow attributes contents =
-    case contents of 
-        [] -> tr attributes []
-        a::c -> tr attributes ((th [] [a])::(List.map (\x -> x) c))
+    case contents of
+        [] ->
+            tr attributes []
 
-type alias BracketRate = (Int,Int)
-type alias Brackets = List BracketRate
+        a :: c ->
+            tr attributes (th [] [ a ] :: List.map (\x -> x) c)
+
+
+type alias BracketRate =
+    ( Int, Int )
+
+
+type alias Brackets =
+    List BracketRate
+
 
 takePercentage : Int -> Int -> Int
 takePercentage number percentage =
     let
-        wholenumber = number*percentage
-        mainNumber = wholenumber // 100
-        rem  = remainderBy 100 wholenumber
-    in
-        if rem >= 50 then
-            mainNumber+1
-        else
-            mainNumber
+        wholenumber =
+            number * percentage
 
-                
+        mainNumber =
+            wholenumber // 100
+
+        rem =
+            remainderBy 100 wholenumber
+    in
+    if rem >= 50 then
+        mainNumber + 1
+
+    else
+        mainNumber
+
+
 calculateTaxesHelper : Brackets -> Int -> ( Int, Int )
 calculateTaxesHelper bracket taxableIncome =
-    let reducer =
-            \(bracketMaxIncome,bracketRate) (income,taxes) ->
+    let
+        reducer =
+            \( bracketMaxIncome, bracketRate ) ( income, taxes ) ->
                 if income > bracketMaxIncome then
                     ( bracketMaxIncome
-                    , taxes+(takePercentage
-                                 (income - bracketMaxIncome)
-                                 bracketRate)
+                    , taxes
+                        + takePercentage
+                            (income - bracketMaxIncome)
+                            bracketRate
                     )
+
                 else
-                    (income,taxes)
+                    ( income, taxes )
     in
-    List.foldl reducer (taxableIncome,0) bracket
-        
+    List.foldl reducer ( taxableIncome, 0 ) bracket
+
+
 calculateTaxes : Brackets -> Int -> Int
 calculateTaxes bracket taxableIncome =
     case calculateTaxesHelper bracket taxableIncome of
-        (_, t) -> t
+        ( _, t ) ->
+            t
+
 
 calculateEffectiveTaxRate : Brackets -> Int -> Int -> Float
 calculateEffectiveTaxRate bracket deduction income =
-    let taxableIncome = if income > deduction
-                        then income - deduction
-                        else 0
+    let
+        taxableIncome =
+            if income > deduction then
+                income - deduction
+
+            else
+                0
     in
-        toFloat (calculateTaxes bracket taxableIncome) / toFloat income
+    toFloat (calculateTaxes bracket taxableIncome) / toFloat income
+
+
 
 -- SUBSCRIPTIONS
+
+
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-  Sub.none
+    Sub.none
--- src/TaxData.elm
+++ src/TaxData.elm
@@ -1,126 +1,168 @@
 module TaxData exposing (..)
+
 import Browser
-import Html exposing (Html, div, table, th, tr, td, tbody, text)
+import Html exposing (Html, div, table, tbody, td, text, th, tr)
 import Html.Attributes exposing (class)
 import String
 
-type alias BracketRate = (Int,Int)
-type alias Brackets = List BracketRate
+
+type alias BracketRate =
+    ( Int, Int )
+
+
+type alias Brackets =
+    List BracketRate
+
+
 type Msg
     = Change String
 
 
-
-formatIntPadded : Int -> String 
+formatIntPadded : Int -> String
 formatIntPadded n =
-    if n < 10 then 
-        "00" ++ (String.fromInt n)
-    else if n < 100 then 
-        "0" ++ (String.fromInt n)
-    else 
-        String.fromInt n 
+    if n < 10 then
+        "00" ++ String.fromInt n
 
-formatIntHelper : List (String) -> Int -> String 
+    else if n < 100 then
+        "0" ++ String.fromInt n
+
+    else
+        String.fromInt n
+
+
+formatIntHelper : List String -> Int -> String
 formatIntHelper result n =
-    if n < 1000 then 
-        (String.fromInt n)::result 
-        |> String.join ","
-    else 
-        let
-            rem = remainderBy 1000 n
-            str = formatIntPadded rem 
-        in
-        
-        formatIntHelper (str::result) (n // 1000)
-        
-formatInt : Int -> String
-formatInt = formatIntHelper []
+    if n < 1000 then
+        String.fromInt n
+            :: result
+            |> String.join ","
 
+    else
+        let
+            rem =
+                remainderBy 1000 n
+
+            str =
+                formatIntPadded rem
+        in
+        formatIntHelper (str :: result) (n // 1000)
+
+
+formatInt : Int -> String
+formatInt =
+    formatIntHelper []
 
 
 taxBracket2023single : Brackets
-taxBracket2023single = [ (578120,37)
-                       , (231250,35)
-                       , (182100,32)
-                       , (95375,24)
-                       , (44725,22)
-                       , (11000,12)
-                       , (0,10)
-                       ]
+taxBracket2023single =
+    [ ( 578120, 37 )
+    , ( 231250, 35 )
+    , ( 182100, 32 )
+    , ( 95375, 24 )
+    , ( 44725, 22 )
+    , ( 11000, 12 )
+    , ( 0, 10 )
+    ]
+
+
 standardDeduction2023single : Int
-standardDeduction2023single = 13850
+standardDeduction2023single =
+    13850
+
 
 dataFormatted : Brackets -> List ( String, String )
 dataFormatted bracket =
     let
-        lowerBound = 
-            bracket 
-            |> List.map (\(x,_) -> "$"++formatInt (x+1))
-        
-        upperBound = 
-            " or more"::(bracket 
-                         |> List.map (\(x,_) -> "$"++formatInt x)
-                         |> List.map (\x -> " to " ++ x))
-        
+        lowerBound =
+            bracket
+                |> List.map (\( x, _ ) -> "$" ++ formatInt (x + 1))
+
+        upperBound =
+            " or more"
+                :: (bracket
+                        |> List.map (\( x, _ ) -> "$" ++ formatInt x)
+                        |> List.map (\x -> " to " ++ x)
+                   )
+
         rates =
             bracket
-            |> List.map (\(_,y) -> formatInt y ++ "%")
+                |> List.map (\( _, y ) -> formatInt y ++ "%")
     in
-        List.map3 (\x y z -> (x,y++z)) 
-            rates lowerBound upperBound
+    List.map3 (\x y z -> ( x, y ++ z ))
+        rates
+        lowerBound
+        upperBound
         |> List.reverse
-    
+
 
 main : Program () Model Msg
 main =
-    Browser.element { init = init
-                     , view = view
-                     , update = update
-                     , subscriptions = subscriptions
-                     }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
 
 -- MODEL
 
-type alias Model = ()
 
-init : () -> (Model, Cmd Msg)
-init _ = ( ()
-         , Cmd.none
-         )
+type alias Model =
+    ()
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( ()
+    , Cmd.none
+    )
+
 
 view : Model -> Html Msg
-view _ = 
-    div [] [ bracketDiv  
-           ]
+view _ =
+    div []
+        [ bracketDiv
+        ]
+
 
 deductionDiv : Html msg
-deductionDiv = 
-    div [] [ text "Standard Deduction: "
-           , text ("$"++formatInt standardDeduction2023single) ]
+deductionDiv =
+    div []
+        [ text "Standard Deduction: "
+        , text ("$" ++ formatInt standardDeduction2023single)
+        ]
+
 
 bracketDiv : Html msg
 bracketDiv =
-    table [class "table"] 
-          [ tr [] [ th [] [text "Tax rate"]
-                  , th [] [text "Income range"]
-                  ]
-          , tbody [] (taxBracketRows (dataFormatted taxBracket2023single))
-          ]
+    table [ class "table" ]
+        [ tr []
+            [ th [] [ text "Tax rate" ]
+            , th [] [ text "Income range" ]
+            ]
+        , tbody [] (taxBracketRows (dataFormatted taxBracket2023single))
+        ]
+
 
 taxBracketRows : List ( String, String ) -> List (Html msg)
-taxBracketRows formattedBracket = 
-    List.map 
-        (\ (x,y) -> tr [] [ td [] [text x]
-                            , td [] [text y]
-                            ])
+taxBracketRows formattedBracket =
+    List.map
+        (\( x, y ) ->
+            tr []
+                [ td [] [ text x ]
+                , td [] [ text y ]
+                ]
+        )
         formattedBracket
+
 
 update : Msg -> b -> ( Model, Cmd msg )
 update _ _ =
-    ( () , Cmd.none )
+    ( (), Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Sub.none 
-
+    Sub.none
